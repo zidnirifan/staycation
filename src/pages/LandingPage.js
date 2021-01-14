42e610291c5859521f6e3a2c57c103a31e53c281
Zidni Rif'an <Zidni Rif'an>
2021-01-14 17:23:57 +0700

    Create Most Picked section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 import Header from 'parts/Header';
 import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = createRef();
+  }
+
   render() {
     return (
       <>
         <Header {...this.props} />
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }

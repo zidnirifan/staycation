e6756f94d5256cbd794b5fbf53fb84ffb5772a03
Zidni Rif'an <Zidni Rif'an>
2021-01-19 13:52:52 +0700

    little fixing

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -15,7 +15,7 @@ export default function MostPicked(props) {
               <div className="card card-featured">
                 <div className="tag">
                   ${item.price}
-                  <span className="font-weight-light">per {item.unit}</span>
+                  <span className="font-weight-light"> per {item.unit}</span>
                 </div>
                 <figure className="img-wrapper">
                   <img
